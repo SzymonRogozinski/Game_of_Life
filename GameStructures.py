6f15a9487fe0b554566227ca43c0d0d34b08abfd
SzymonRogozinski <pokeszymon.pl@gmail.com>
2024-05-12 21:46:38 +0200

    change data structures

--- GameStructures.py
+++ GameStructures.py
@@ -1,12 +1,17 @@
 
 class Game:
 
-    def __init__(self, height, width, iteration_number, iterations, threads):
+    def __init__(self, height, width, iteration_number, threads):
         self.height = height
         self.width = width
         self.iteration_number = iteration_number
-        self.iterations = iterations
+        self.iterations = []
         self.threads = threads
+
+    def load_data(self, filename):
+        i = Iteration(0, 0)
+        i.load_file(filename)
+        self.iterations.append(i)
 
 
 class Iteration:
@@ -14,3 +19,6 @@ class Iteration:
     def __init__(self, height, width):
         self.plane = [[False for _ in range(width)] for _ in range(height)]
 
+    def load_file(self, filename):
+        with open(filename, "r") as file:
+            self.plane = [[s == "1" for s in line[:-1]] for line in file.readlines()]
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from GameStructures import Iteration
 
 if __name__ == '__main__':
-    xd = Iteration(10,10)
+    xd = Iteration(0,0)
+    xd.load_file("data.txt")
+
     print(xd)
 
 
